c90391981bdf6d9e262a2ef08277103afc6badbc
keliworkcontact-wq <keli.work.contact@gmail.com>
2026-03-29 14:49:23 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom' // 必须是 HashRouter
-import App from './App' // 或者是你的主页面组件
+import { HashRouter as Router, Routes, Route } from 'react-router-dom' 
+import App from './App' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
